0c949bd1e25656b836e1cec7974a7c6147afbdea
Andreas Jensen <s235455@dtu.dk>
2023-09-19 15:48:39 +0200

    Andreas commit på development, hotfix af Die.java

--- Die.java
+++ Die.java
@@ -1,10 +1,16 @@
 public class Die {
     public int value;
 
-    public void GetValue() {
+    public void roll() {
 
         var val = java.lang.Math.random() * 6;
         value = (int) val;
+    }
+
+    public int getValue() {
+
+        return value;
+
     }
 
 }
