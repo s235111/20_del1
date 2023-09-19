706ca698a2cba5cc735743e589f9164d6fe93adb
PolyZone <sorenn.dk@gmail.com>
2023-09-19 16:17:47 +0200

    Dice Cup Working

--- DiceCup.java
+++ DiceCup.java
@@ -1,21 +1,17 @@
 
 public class DiceCup {
 
-	public static void main(String[] args) {
-		var cup = new DiceCup();
-
-		System.out.println(cup.getSum());
-
-		System.out.println(cup.die1.getValue());
-		System.out.println(cup.die2.getValue());
-	}
 
 	Die die1 = new Die();
 	Die die2 = new Die();
 
-	public int getSum() {
+	public void rollDice() {
 		die1.roll();
 		die2.roll();
+
+	}
+
+	public int getSum() {
 
 		var sum = die1.getValue() + die2.getValue();
 
