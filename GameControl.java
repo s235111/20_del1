a4dcc1ecff4ba93e66726bb3604a9b6999096e24
NN0323 <noah@dimse.dk>
2023-09-21 13:10:14 +0200

    Add points to total score and win condition

--- GameControl.java
+++ GameControl.java
@@ -1,4 +1,31 @@
 class GameControl {
+
+	Player player1 = new Player();
+	Player player2 = new Player();
+
+	DiceCup diceCup = new DiceCup();
+
+	public void playTurn(Player player){
+
+		diceCup.rollDice();
+		var sum = diceCup.getSum();
+
+		System.out.println("you rolled " + sum);
+
+		player.addPoints(sum);
+
+		System.out.println("Your total points are " + player.getPoints());
+
+	}
+
+	public boolean checkWin(Player player){
+
+		if (player.getPoints() >= 40){
+			System.out.println(player.getName() + " wins");
+		}
+
+	}
+
 	public static void main(String[] args) {
 
 
